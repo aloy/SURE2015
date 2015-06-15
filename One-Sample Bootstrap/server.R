c9ba936e884b5ae608ac2dcdbaeb9b9d711435e5
adamisch <alexandra.damisch@gmail.com>
2015-06-15 16:13:59 -0500

    Simple semi-responsive bootstrap
    Bootstrap works and responds to mean/median/standard deviation radio
    buttons, but not done in ggvis and not responsive to binwidth/number of
    bootstraps

--- One-Sample Bootstrap/server.R
+++ One-Sample Bootstrap/server.R
@@ -6,32 +6,37 @@ shinyServer(function(input,output){
   
   data(SleepStudy)
   original_data <- SleepStudy$AverageSleep
-
-trials <- function(x, list) {
-switch(list,
-       bootMean = do(1000) * mean(sample(original_data, replace = TRUE)),
-       med = do(1000) * median(sample(original_data, replace = TRUE)),
-       sdev = do(1000) * sd(sample(original_data, replace = TRUE))
-  )
-}
-
-output$bootHist <- renderPlot({
-  result <- trials$result
-  hist(result)
-})
+  
+  trialsMean <- do(1000) * mean(sample(original_data, replace = TRUE))
+  trialsMedian <- do(1000) * median(sample(original_data, replace = TRUE))
+  trialsSdev <- do(1000) * sd(sample(original_data, replace = TRUE))
+  
+  trialsFunction <- function(x, list) {
+    switch(list,
+           bootMean = trialsMean,
+           med = trialsMedian,
+           sdev = trialsSdev
+    )
+  }
+  
+  output$bootHist <- renderPlot({
+    stat <- input$stat
+    hist(trialsFunction(original_data, input$stat)$result, col = 'darkgray', border = 'white', 
+         xlab=stat, main=paste("Bootstrap of", stat))
+  })
   
   output$origHist <- renderPlot({
-   hist(original_data, col = 'darkgray', border = 'white', xlab="Count", 
-        ylab="Hours of Sleep", main=paste("Original Sample"))
+    hist(original_data, col = 'darkgray', border = 'white', xlab="Hours of Sleep",
+         ylab="Frequency", main="Original Sample")
   })
-   output$summary <- renderPrint({
-     summary(original_data)
-   })
+  output$summary <- renderPrint({
+    summary(original_data)
+  })
   output$mean <- renderPrint({
     mean(original_data)
   })
   output$sd <- renderPrint({
     sd(original_data)
   })
-
+  
 })
--- One-Sample Bootstrap/ui.R
+++ One-Sample Bootstrap/ui.R
@@ -2,44 +2,43 @@ shinyUI(fluidPage(
   titlePanel("One-Sample Bootstrapping"),
   fluidRow(
     column(2,
-          wellPanel("Sidebar, presumably with some description about the app.")
+           wellPanel("Sidebar, presumably with some description about the app.")
     ),
     column(3,
            wellPanel(h3("Bootstrap Control Panel"),
-           radioButtons("plot", label=h4("Plotting"),
-            c("Histogram" = 1, "Density" = 2), selected=1),
-           numericInput("w", 
-                        label = h5("Bin Width"), 
-                        value = 0.025),
-           h4("Resampling"),
-           numericInput("num", 
-                        label = h5("Number of Bootstraps"), 
-                        value = 1000, min = 1, max = 100000),
-           radioButtons("stat", label = h5("Statistic"),
-                        c("Mean" = "bootMean", "Median" = "med", "Standard Deviation" = "sdev"), 
-                        selected = "bootMean"),
-           radioButtons("ci", label = h5("Confidence Interval"),
-                        c("Percentile" = 6, "Normal-Based" = 7), selected = 6),
-           numericInput("level", 
-                        label = h5("Confidence Level"), 
-                        value = 0.95, min = 0.01, max = 0.99))
+                     radioButtons("plot", label=h4("Plotting"),
+                                  c("Histogram" = 1, "Density" = 2), selected=1),
+                     numericInput("w", 
+                                  label = h5("Bootstrap Bin Width"), 
+                                  value = 0.025, step=0.05, min = 0.05),
+                     h4("Resampling"),
+                     numericInput("num", 
+                                  label = h5("Number of Bootstraps"), 
+                                  value = 1000, min = 1, max = 100000),
+                     radioButtons("stat", label = h5("Statistic"),
+                                  c("Mean" = "bootMean", "Median" = "med", "Standard Deviation" = "sdev"), 
+                                  selected = "bootMean"),
+                     radioButtons("ci", label = h5("Confidence Interval"),
+                                  c("Percentile" = 6, "Normal-Based" = 7), selected = 6),
+                     numericInput("level", 
+                                  label = h5("Confidence Level"), 
+                                  value = 0.95, min = 0.01, max = 0.99))
     ),
     column(3,
-      wellPanel(h3("Original Sample"),
-      plotOutput("origHist"),
-      h5("Summary Statistics"),
-        h6("Five-Number Summary"),
-      verbatimTextOutput("summary"),
-      h6("Mean"),
-        verbatimTextOutput("mean"),
-      h6("Standard Deviation"),
-       verbatimTextOutput("sd"))
+           wellPanel(h3("Original Sample"),
+                     plotOutput("origHist"),
+                     h5("Summary Statistics"),
+                     h6("Five-Number Summary"),
+                     verbatimTextOutput("summary"),
+                     h6("Mean"),
+                     verbatimTextOutput("mean"),
+                     h6("Standard Deviation"),
+                     verbatimTextOutput("sd"))
     ),
-  column(4,
-         wellPanel(h3("Bootstrap Distribution"),
-         plotOutput("bootHist"),
-         h5("Summary Statistics")
-         )
+    column(4,
+           wellPanel(h3("Bootstrap Distribution"),
+                     plotOutput("bootHist")
+           )
     )
-)
+  )
 ))
