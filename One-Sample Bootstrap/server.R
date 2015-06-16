d42b9845a4e173e59121a22d32f18e94c188959c
adamisch <alexandra.damisch@gmail.com>
2015-06-16 09:37:34 -0500

    GG VIs Bootstraps, no CIs
    Bootstrap hist and kernel densities done, although rendering in viewer
    and not in external window. On to CIs.

--- One-Sample Bootstrap/server.R
+++ One-Sample Bootstrap/server.R
@@ -7,27 +7,9 @@ shinyServer(function(input,output){
   data(SleepStudy)
   original_data <- SleepStudy$AverageSleep
   
-  trialsMean <- do(1000) * mean(sample(original_data, replace = TRUE))
-  trialsMedian <- do(1000) * median(sample(original_data, replace = TRUE))
-  trialsSdev <- do(1000) * sd(sample(original_data, replace = TRUE))
-  
-  trialsFunction <- function(x, list) {
-    switch(list,
-           bootMean = trialsMean,
-           med = trialsMedian,
-           sdev = trialsSdev
-    )
-  }
-  
-  output$bootHist <- renderPlot({
-    stat <- input$stat
-    hist(trialsFunction(original_data, input$stat)$result, col = 'darkgray', border = 'white', 
-         xlab=stat, main=paste("Bootstrap of", stat))
-  })
-  
   output$origHist <- renderPlot({
-    hist(original_data, col = 'darkgray', border = 'white', xlab="Hours of Sleep",
-         ylab="Frequency", main="Original Sample")
+    w <- input$w
+    qplot(original_data, xlab="Hours of Sleep", ylab="Frequency", main="Original Sample",)
   })
   output$summary <- renderPrint({
     summary(original_data)
@@ -37,6 +19,46 @@ shinyServer(function(input,output){
   })
   output$sd <- renderPrint({
     sd(original_data)
+  })
+  
+  trialsMean <- do(1000) * mean(sample(original_data, replace = TRUE))
+  trialsMedian <- do(1000) * median(sample(original_data, replace = TRUE))
+  trialsSdev <- do(1000) * sd(sample(original_data, replace = TRUE))
+  
+  trialsFunction <- function(x, list) {
+    switch(list,
+           bootMean = trialsMean,
+           bootMedian = trialsMedian,
+           bootStdDev = trialsSdev
+    )
+  }
+
+  plotType <- function(x, type) {
+    switch(type,
+           his =  trialsFunction(original_data, input$stat) %>% 
+             ggvis(~result) %>% 
+             layer_histograms(binwidth = input$w) %>% 
+             add_axis("x", title = paste("Bootstrap of", input$stat)),
+           den = trialsFunction(original_data, input$stat) %>% 
+             ggvis(~result) %>% 
+             layer_densities() %>%
+             add_axis("x", title = paste("Kernel Bootstrap of", input$stat))
+   )}
+  
+  output$ggBoot <- renderPlot({
+    plotType(original_data, input$plot)
+  })
+  
+  output$bootSummary <- renderPrint({
+    summary(trialsFunction(original_data, input$stat)$result)
+  })
+
+  output$bootBias <- renderPrint({
+    mean(original_data) - mean(trialsFunction(original_data, input$stat)$result)
+  })
+  
+  output$bootSd <- renderPrint({
+    sd(trialsFunction(original_data, input$stat)$result)
   })
   
 })
