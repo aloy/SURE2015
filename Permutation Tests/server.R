50c2dd6e93611e6e862709d68e60d3504afcfc25
adamisch <alexandra.damisch@gmail.com>
2015-07-02 22:04:34 -0500

    Now just doesn’t do anything when you change bootstrap number, instead of quitting.

--- Permutation Tests/server.R
+++ Permutation Tests/server.R
@@ -75,11 +75,10 @@ output$observedDiff <- renderText({
 diff()$mean.diff
 })
 
-
 trials <- reactive({
+n <- input$num
 dataSize <- nrow(simdata())
 size <- nrow(simdata())/2
-n <- input$num
 toSample <- simdata()$Quantitative..
 result <- matrix(nrow=n)
 for (i in 1:n){
@@ -91,15 +90,13 @@ as.data.frame(result)
 })
 
 output$pval <- renderText({
-n <- input$num
 (sum(trials()$result >= diff()$mean.diff) +1)/(n+1)
-})
+  })
 
 observe({
-  n <- input$num
   probabilities <- (1:n)/(1+n)
-  normal.quantiles <- qnorm(probabilities, mean(result, na.rm = T), sd(result, na.rm = T))
-  qqdata <- data.frame(sort(normal.quantiles), sort(result))
+normal.quantiles <- qnorm(probabilities, mean(result, na.rm = T), sd(result, na.rm = T))
+qqdata <- data.frame(sort(normal.quantiles), sort(result))
 ggSwitch <- switch(input$type,
   his = as.data.frame(result) %>% 
     ggvis(~result) %>% 
@@ -108,8 +105,7 @@ ggSwitch <- switch(input$type,
     add_axis("y", title="Count") %>%
     add_axis("x", title="Permutation Distribution", ticks=0, orient="top", properties = axis_props(
       axis = list(stroke = "white"),
-      labels = list(fontSize = 0))) %>% 
-    bind_shiny("trialsHist2", "trialsHist2_ui"),
+      labels = list(fontSize = 0))),
   den = as.data.frame(result) %>% 
     ggvis(~result) %>% 
     layer_densities() %>%
@@ -117,16 +113,14 @@ ggSwitch <- switch(input$type,
     add_axis("y", title="Density") %>%
     add_axis("x", title="Permutation Distribution", ticks=0, orient="top", properties = axis_props(
       axis = list(stroke = "white"),
-      labels = list(fontSize = 0))) %>% 
-    bind_shiny("trialsHist2", "trialsHist2_ui"),
+      labels = list(fontSize = 0))), 
   qq = qqdata %>% 
     ggvis(~sort.normal.quantiles., ~sort.result.) %>% 
     layer_points() %>% 
     add_axis("x", title="Theoretical") %>%
-    add_axis("y", title="Sample") %>%
-    bind_shiny("trialsHist2", "trialsHist2_ui")
+    add_axis("y", title="Sample")
   )
-ggSwitch
+ggSwitch %>% bind_shiny("trialsHist2", "trialsHist2_ui")
 })
 
 })
