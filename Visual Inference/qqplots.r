c2ea16c0bd2d5bd790e532730c2dd66e354cceee
adamisch <alexandra.damisch@gmail.com>
2015-07-31 14:42:09 -0500

    Fixed issue with adjusted plots without bands

--- Visual Inference/qqplots.r
+++ Visual Inference/qqplots.r
@@ -201,10 +201,10 @@ rot_none <- function(dframe) { #Ordinary Detrended without bands
   require(ggplot2)
   require(grid)
   ggplot(aes(x = x, y = y - fit), data = dframe) + 
-    geom_hline(yintercept=0, colour="grey30")+
+    facet_wrap(~ .sample, ncol = 5) + 
+    geom_hline(yintercept = 0, colour = "grey30")+
     geom_point() + 
-    theme_bw() + 
-    facet_wrap(~.sample, ncol=5) +
+    theme_bw() +
     labs(x = "", y = "") +
     theme(axis.text.y = element_blank(), axis.text.x = element_blank(),
           axis.ticks = element_blank(), plot.margin=grid::unit(c(0, 0, 0, 0), "cm"))
@@ -214,10 +214,10 @@ rot2_none <- function(dframe) { #Adjusted detrended without bands
   require(ggplot2)
   require(grid)
   ggplot(aes(x = x, y = y - fit), data = dframe) + 
+    facet_wrap(~.sample, ncol=5) +
     geom_hline(yintercept=0, colour="grey30")+
     geom_point() + 
     theme_bw() + 
-    facet_wrap(~.sample, ncol=5) +
     labs(x = "", y = "") +
     theme(axis.text.y = element_blank(), axis.text.x = element_blank(),
           axis.ticks = element_blank(), plot.margin=grid::unit(c(0, 0, 0, 0), "cm"))
